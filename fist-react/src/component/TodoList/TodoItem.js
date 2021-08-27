9efbea8f15411b88bc035186db8f50a3e9afe607
anhlhde140207 <59632403+anhlhde140207@users.noreply.github.com>
2021-08-27 23:29:50 +0700

    delete

--- fist-react/src/component/TodoList/TodoItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function TodoItem(props) {
-  return (
-    <div>
-      <li onClick={() => props.deleteItem(props.id)}>{props.listItem}</li>
-    </div>
-  );
-}
